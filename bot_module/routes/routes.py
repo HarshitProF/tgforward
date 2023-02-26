b0e190fc1328b2130787832ff21811d0bcf79075
HarshitProF <121898573+HarshitProF@users.noreply.github.com>
2023-02-26 15:25:28 +0000

    Update routes.py

--- bot_module/routes/routes.py
+++ bot_module/routes/routes.py
@@ -13,8 +13,9 @@ async def send(event):
             except Exception as e:
                 print(e)
                 await client.send_message("@LinkConvertTerabot",event.original_update.message)
-            time.sleep(2)
+            time.sleep(10)
             message_recieved=await client.get_messages("@LinkConvertTerabot",limit=1)
+            print(massage_recieved)
             await client.forward_messages(1674861391,message_recieved)
     except:
         pass
